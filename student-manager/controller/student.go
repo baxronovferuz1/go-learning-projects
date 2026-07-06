1f73eb4d4ff09dfb28b73cddb450198a7591470c
baxronovferuz1 <baxronovvferuz25@gmail.com>
2026-07-06 17:59:08 +0500

    add DeletedStudent function

--- student-manager/controller/student.go
+++ student-manager/controller/student.go
@@ -110,3 +110,27 @@ func UpdateStudent(c *gin.Context) {
 		"data":    student,
 	})
 }
+
+func DeletedStudent(c *gin.Context) {
+	id := c.Param("id")
+
+	var student models.Student
+
+	if err := database.DB.First(&student, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Student not found",
+		})
+		return
+	}
+
+	if err := database.DB.Delete(&student).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to delete student",
+		})
+		return
+
+	}
+	c.JSON((http.StatusOK), gin.H{
+		"message": "Student successfully deleted",
+	})
+}
