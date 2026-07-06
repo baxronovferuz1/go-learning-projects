caf3004c1338a9f180724871d8be6c021c869fdc
baxronovferuz1 <baxronovvferuz25@gmail.com>
2026-07-06 17:36:11 +0500

    GetStudentByID and UpdateStudent func

--- student-manager/controller/student.go
+++ student-manager/controller/student.go
@@ -43,9 +43,9 @@ func CreateStudent(c *gin.Context) {
 }
 
 func GetStudents(c *gin.Context) {
-	var student []models.Student
+	var students []models.Student
 
-	if err := database.DB.Find(&student).Error; err != nil {
+	if err := database.DB.Find(&students).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"error": "Failed to fetch student",
 		})
@@ -53,6 +53,60 @@ func GetStudents(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Student fetched succesfully",
+		"data":    students,
+	})
+}
+
+func GetStudentByID(c *gin.Context) {
+	id := c.Param("id")
+
+	var student models.Student
+
+	if err := database.DB.First(&student, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Student not found",
+		})
+		return
+
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Successfully found",
+		"data":    student,
+	})
+}
+
+func UpdateStudent(c *gin.Context) {
+	id := c.Param("id")
+
+	var student models.Student
+
+	if err := database.DB.First(&student, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Student not found",
+		})
+		return
+	}
+
+	var input models.Student
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	student.FullName = input.FullName
+	student.Phone_number = input.Phone_number
+	student.Email = input.Email
+
+	if err := database.DB.Save(&student).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to upadte student",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "student uptated successfully",
 		"data":    student,
 	})
 }
