656ff807b53d4c0a641eeaa3f40d79c3ba42cfda
baxronovferuz1 <baxronovvferuz25@gmail.com>
2026-07-10 12:58:36 +0500

    commit

---
+++ library-manager/models/book.go
@@ -0,0 +1,10 @@
+package models
+
+type Book struct {
+	ID            uint   `json:"id" gorm:"primaryKey"`
+	Title         string `json:"title" gorm:"not null"`
+	Author        string `json:"author" gorm:"not null"`
+	Category      string `json:"category" gorm:"not null"`
+	PublisherYear int    `json:"publisher_year" gorm:"not null"`
+	Available     bool   `json:"available" gorm:"default:true"`
+}
