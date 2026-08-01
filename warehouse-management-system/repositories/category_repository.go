e9ec9492112ca3ba0e352fc24df5a7179e4bc4ac
baxronovferuz1 <baxronovvferuz25@gmail.com>
2026-08-01 23:14:40 +0500

    category repository

---
+++ warehouse-management-system/repositories/category_repository.go
@@ -0,0 +1,48 @@
+package repositories
+
+import (
+	"warehouse-management-system/models"
+
+	"gorm.io/gorm"
+)
+
+type CategoryRepository struct {
+	db *gorm.DB
+}
+
+func NewCategoryRepository(db *gorm.DB) *CategoryRepository {
+	return &CategoryRepository{
+		db: db,
+	}
+}
+
+func (r *CategoryRepository) Create(category *models.Category) error {
+	return r.db.Create(category).Error
+}
+
+func (r *CategoryRepository) GetAll() ([]models.Category, error) {
+	var categories []models.Category
+
+	err := r.db.Find(&categories).Error
+
+	return categories, err
+}
+
+func (r *CategoryRepository) GetByID(id uint) (*models.Category, error) {
+	var category models.Category
+
+	err := r.db.First(&category, id).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &category, nil
+}
+
+func (r *CategoryRepository) Update(category *models.Category) error {
+	return r.db.Save(category).Error
+}
+
+func (r *CategoryRepository) Delete(id uint) error {
+	return r.db.Delete(&models.Category{}, id).Error
+}
