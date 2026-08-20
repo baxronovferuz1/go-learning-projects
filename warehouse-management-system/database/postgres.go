0cb63d5d357bca3c07f7cbd0d1da3342e091ea2f
baxronovferuz1 <baxronovvferuz25@gmail.com>
2026-08-20 11:31:30 +0500

    database

--- warehouse-management-system/database/postgres.go
+++ warehouse-management-system/database/postgres.go
@@ -1,1 +1,33 @@
 package database
+
+import (
+	"fmt"
+	"log"
+
+	"warehouse-management-system/config"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDB(cfg *config.Config) *gorm.DB {
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		cfg.DBHost,
+		cfg.DBPort,
+		cfg.DBUser,
+		cfg.DBPassword,
+		cfg.DBName,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Database connection failed:", err)
+	}
+
+	log.Println("Database connected successfully")
+
+	return db
+}
